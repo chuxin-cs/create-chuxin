aa90fd12eed8835543497dc24268f72652d36f29
chuxin <15115359055@139.com>
2024-06-14 17:41:03 +0800

    feat: 新增about组件

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   routes: [
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
+    { path: "/about", component: "About" },
   ],
-  npmClient: 'pnpm',
+  npmClient: "pnpm",
 });
---
+++ src/pages/About/index.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+  return (
+    <div>
+      <p>This is umi AboutPage.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
